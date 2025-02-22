11b1efa579abf251d5da24aecab25c6d216a21a0
DragomirPetko <dramppet@abv.bg>
2025-02-22 09:06:50 +0200

    add two task

---
+++ 01.JS Syntax Fundamentals/ex/07.charstostring.js
@@ -0,0 +1,19 @@
+function solve(first, second, third) {
+    let charOfSymbols = first + second + third
+    console.log(charOfSymbols)
+}
+
+solve('a',
+    'b',
+    'c'
+)
+
+solve('%',
+    '2',
+    'o'
+)
+
+solve('1',
+    '5',
+    'p'
+)
---
+++ 01.JS Syntax Fundamentals/ex/08.reversedchars.js
@@ -0,0 +1,14 @@
+function solve(first, second, third) {
+    console.log(`${third} ${second} ${first}`)
+}
+
+
+solve('A',
+    'B',
+    'C'
+)
+
+solve('1',
+    'L',
+    '&'
+)
