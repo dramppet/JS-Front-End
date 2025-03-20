89963c0e10fda23410009f466038aa69adee2ab3
DragomirPetko <dramppet@abv.bg>
2025-03-20 20:10:57 +0200

    fixed

--- 5.DOM Intoduction/04. Search in List/search-in-list.js
+++ 5.DOM Intoduction/04. Search in List/search-in-list.js
@@ -1,17 +1,21 @@
 function solve() {
    let list = document.getElementById('towns');
-   let input = document.getElementById('searchText').value;
+   let input = document.getElementById('searchText');
    let output = document.getElementById('result');
 
    let pattern = input.value;
+
+   let matches = 0;
 
    for(let row of list.children){
       if(row.textContent.includes(pattern)){
          row.style.fontWeight = 'bold';
          row.style.textDecoration = 'inderline';
+         matches ++;
       }else{
          row.style.fontWeight = ' ';
          row.style.textDecoration = ' ';
       }
    }
+   output.textContent = `${matches} matches found`;
 }
